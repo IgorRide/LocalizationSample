26069f8d79f6f71d5f4c01645b1eae2af8662026
Igor Ride <igorride@gmail.com>
2016-07-18 21:52:33 +0300

    first

---
+++ load_a_web_page/WebViewController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using CoreGraphics;
+using System.Linq;
+using Foundation;
+using UIKit;
+
+namespace WebView {
+
+	public class WebViewController : UIViewController {
+		
+		UIWebView webView;
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			
+			Title = "WebView";
+			View.BackgroundColor = UIColor.White;
+
+			webView = new UIWebView(View.Bounds);			
+			View.AddSubview(webView);
+
+			var url = "https://xamarin.com/account/login"; // NOTE: https required for iOS 9 ATS
+			webView.LoadRequest (new NSUrlRequest (new NSUrl (url)));
+			
+			// if this is false, page will be 'zoomed in' to normal size
+			//webView.ScalesPageToFit = true;
+
+
+			// iOS 9 ATS docs
+			// http://developer.xamarin.com/guides/ios/platform_features/introduction_to_ios9/ats/
+		}
+		public override void ViewWillAppear(bool animated)
+		{
+			base.ViewWillAppear(animated);
+			webView.Frame = View.Frame;
+		}
+	}
+}
